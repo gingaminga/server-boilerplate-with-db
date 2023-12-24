38b52aaa566fbe5e66828509429340590787ffa9
gingaminga <cjhwaan@gmail.com>
2023-12-24 21:04:52 +0900

    🚩 Feat: 요청 로깅 미들웨어 추가
    요청 시 미들웨어에서 요청 정보를 로깅

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
+import requestLoggingMiddleware from "@middlewares/request-logging.middleware";
 import express from "express";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(requestLoggingMiddleware);
 
 /**
  * @description 상태 체크 API
---
+++ src/middlewares/request-logging.middleware.ts
@@ -0,0 +1,80 @@
+import logger from "@utils/logger";
+import colors from "ansi-colors";
+import { NextFunction, Request, Response } from "express";
+import _ from "lodash";
+import onFinished from "on-finished";
+
+/**
+ * @description 파라미터에 대한 정보 가져오기
+ * @param query query params
+ * @param body body params
+ * @param param param params
+ * @returns 파리미터 로그
+ */
+const getParamsLog = (query?: object, body?: object, param?: object) => {
+  let paramLog = "";
+
+  if (!_.isEmpty(query)) {
+    paramLog += `\n Query params : ${JSON.stringify(query)}`;
+  }
+
+  if (!_.isEmpty(body)) {
+    paramLog += `\n Body params : ${JSON.stringify(body)}`;
+  }
+
+  if (!_.isEmpty(param)) {
+    paramLog += `\n Param params : ${JSON.stringify(param)}`;
+  }
+
+  return paramLog;
+};
+
+/**
+ * @description 요청 시작 시간 로그 남기기
+ * @param url 요청 url
+ * @param method HTTP 메소드
+ * @param query query params
+ * @param body body params
+ * @param param param params
+ */
+const requestStartTimeLog = (url: string, method: string, query?: object, body?: object, param?: object) => {
+  let startLog = `Started [${method}] ${url}`;
+
+  const paramsLog = getParamsLog(query, body, param);
+  startLog += paramsLog;
+
+  logger.info(colors.magenta(startLog));
+};
+
+/**
+ * @description 요청 종료 시간 로그 남기기
+ * @param url 요청 url
+ * @param method HTTP 메소드
+ * @param takenTime 소요 시간
+ */
+const requestFinishTimeLog = (url: string, method: string, takenTime: number) => {
+  const endLog = colors.magenta(`Finished [${method}] ${url} (${takenTime}ms)`);
+  logger.info(endLog);
+};
+
+/**
+ * @description 요청 정보를 로그로 남기는 미들웨어
+ */
+export default (req: Request, res: Response, next: NextFunction) => {
+  const { body, method, originalUrl, params, protocol, query } = req;
+  const host = req.get("host");
+
+  const url = `${protocol}://${host}${originalUrl}`;
+  const startTime = new Date().getTime();
+
+  requestStartTimeLog(url, method, query, body, params);
+
+  onFinished(res, () => {
+    const finishTime = new Date().getTime();
+    const takenTime = finishTime - startTime; // 소요시간
+
+    requestFinishTimeLog(url, method, takenTime);
+  });
+
+  next();
+};
