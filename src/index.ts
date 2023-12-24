36253eb7b5fbb1f7a983d610f71f711839adb2d7
gingaminga <cjhwaan@gmail.com>
2023-12-24 16:53:09 +0900

    🩹 Fix: 변수명 변경

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import "@utils/env";
 
 import app from "@app";
-import { PROJECT_SETTING } from "@utils/constants";
+import { PROJECT } from "@utils/constants";
 import http from "http";
 
-const { PORT } = PROJECT_SETTING;
+const { PORT } = PROJECT;
 
 const server = http.createServer(app);
 
