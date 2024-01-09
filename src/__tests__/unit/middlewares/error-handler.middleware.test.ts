f7b925160bf3d7fd02bd12d324d40d9da0ad9d8e
gingaminga <cjhwaan@gmail.com>
2024-01-10 00:29:11 +0900

    ✅ Test: 테스트 로직 추가
    info.service.ts
    error-handler.middleware.ts

---
+++ src/__tests__/unit/middlewares/error-handler.middleware.test.ts
@@ -0,0 +1,41 @@
+import errorHandlerMiddleware from "@middlewares/error-handler.middleware";
+import { HTTP_STATUS_CODE } from "@utils/constants";
+import ERROR_MESSAGE from "@utils/error-message";
+import { Request, Response } from "express";
+import joi from "joi";
+
+describe(`Error handler middleware test :)`, () => {
+  const req = {} as Request;
+  const res = {
+    error: jest.fn(),
+  } as unknown as Response;
+  const next = jest.fn();
+
+  it(`should be ${HTTP_STATUS_CODE.INVALID_VALUE} error.`, () => {
+    // given
+    const error = new Error("Validate error!");
+    const errorMessage = `${ERROR_MESSAGE.INVALID_VALUE} ${error.message}`;
+    jest.spyOn(joi, "isError").mockReturnValue(true);
+
+    // when
+    errorHandlerMiddleware(error, req, res, next);
+
+    // then
+    expect(res.error).toHaveBeenCalled();
+    expect(res.error).toHaveBeenCalledWith(HTTP_STATUS_CODE.INVALID_VALUE, errorMessage);
+  });
+
+  it(`should be ${HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR} error.`, () => {
+    // given
+    const error = new Error("Internal server error!");
+    const errorMessage = `${ERROR_MESSAGE.INTERNAL_SERVER_ERROR} ${error.message}`;
+    jest.spyOn(joi, "isError").mockReturnValue(false);
+
+    // when
+    errorHandlerMiddleware(error, req, res, next);
+
+    // then
+    expect(res.error).toHaveBeenCalled();
+    expect(res.error).toHaveBeenCalledWith(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR, errorMessage);
+  });
+});
---
+++ src/__tests__/unit/services/info.service.test.ts
@@ -0,0 +1,165 @@
+import errorHandlerMiddleware from "@middlewares/error-handler.middleware";
+import { InfoService } from "@services/info.service";
+import { HTTP_STATUS_CODE } from "@utils/constants";
+import ERROR_MESSAGE from "@utils/error-message";
+import os from "os";
+
+jest.mock("os");
+const mockedHostnameFn = jest.mocked(os.hostname);
+
+describe(`Info service test :)`, () => {
+  describe(`info method test :)`, () => {
+    let infoService: InfoService;
+    let memory: boolean;
+    let name: boolean;
+    let uptime: boolean;
+    const FREE_MEMORY = 10;
+    const TOTAL_MEMORY = 100;
+    const HOST_NAME = "TEST";
+    const UPTIME = 500;
+
+    beforeEach(() => {
+      mockedHostnameFn.mockReturnValue(HOST_NAME);
+      infoService = new InfoService();
+      memory = false;
+      name = false;
+      uptime = false;
+    });
+
+    it(`should only memory info`, () => {
+      // given
+      memory = true;
+
+      const freememSpy = jest.spyOn(os, "freemem").mockReturnValue(FREE_MEMORY);
+      const totalmemSpy = jest.spyOn(os, "totalmem").mockReturnValue(TOTAL_MEMORY);
+
+      // when
+      const serverInfo = infoService.info(memory, name, uptime);
+
+      // then
+      expect(freememSpy).toHaveBeenCalled();
+      expect(totalmemSpy).toHaveBeenCalled();
+      expect(serverInfo.freeMemory).toBe(FREE_MEMORY);
+      expect(serverInfo.totalMemory).toBe(TOTAL_MEMORY);
+      expect(serverInfo.hostName).toBeUndefined();
+      expect(serverInfo.uptime).toBeUndefined();
+    });
+
+    it(`should only name info`, () => {
+      // given
+      name = true;
+
+      // when
+      const serverInfo = infoService.info(memory, name, uptime);
+
+      // then
+      expect(serverInfo.hostName).toBe(HOST_NAME);
+      expect(serverInfo.freeMemory).toBeUndefined();
+      expect(serverInfo.totalMemory).toBeUndefined();
+      expect(serverInfo.uptime).toBeUndefined();
+    });
+
+    it(`should only uptime info`, () => {
+      // given
+      uptime = true;
+
+      const uptimeSpy = jest.spyOn(os, "uptime").mockReturnValue(UPTIME);
+
+      // when
+      const serverInfo = infoService.info(memory, name, uptime);
+
+      // then
+      expect(uptimeSpy).toHaveBeenCalled();
+      expect(serverInfo.uptime).toBe(UPTIME);
+      expect(serverInfo.freeMemory).toBeUndefined();
+      expect(serverInfo.totalMemory).toBeUndefined();
+      expect(serverInfo.hostName).toBeUndefined();
+    });
+
+    it(`should memory, name info`, () => {
+      // given
+      memory = true;
+      name = true;
+
+      const freememSpy = jest.spyOn(os, "freemem").mockReturnValue(FREE_MEMORY);
+      const totalmemSpy = jest.spyOn(os, "totalmem").mockReturnValue(TOTAL_MEMORY);
+      mockedHostnameFn.mockReturnValue("TEST");
+
+      // when
+      const serverInfo = infoService.info(memory, name, uptime);
+
+      // then
+      expect(freememSpy).toHaveBeenCalled();
+      expect(totalmemSpy).toHaveBeenCalled();
+      expect(serverInfo.freeMemory).toBe(FREE_MEMORY);
+      expect(serverInfo.totalMemory).toBe(TOTAL_MEMORY);
+      expect(serverInfo.hostName).toBe(HOST_NAME);
+      expect(serverInfo.uptime).toBeUndefined();
+    });
+
+    it(`should memory, uptime info`, () => {
+      // given
+      memory = true;
+      uptime = true;
+
+      const freememSpy = jest.spyOn(os, "freemem").mockReturnValue(FREE_MEMORY);
+      const totalmemSpy = jest.spyOn(os, "totalmem").mockReturnValue(TOTAL_MEMORY);
+      const uptimeSpy = jest.spyOn(os, "uptime").mockReturnValue(UPTIME);
+
+      // when
+      const serverInfo = infoService.info(memory, name, uptime);
+
+      // then
+      expect(freememSpy).toHaveBeenCalled();
+      expect(totalmemSpy).toHaveBeenCalled();
+      expect(uptimeSpy).toHaveBeenCalled();
+      expect(serverInfo.freeMemory).toBe(FREE_MEMORY);
+      expect(serverInfo.totalMemory).toBe(TOTAL_MEMORY);
+      expect(serverInfo.uptime).toBe(UPTIME);
+      expect(serverInfo.hostName).toBeUndefined();
+    });
+
+    it(`should name, uptime info`, () => {
+      // given
+      name = true;
+      uptime = true;
+
+      mockedHostnameFn.mockReturnValue("TEST");
+      const uptimeSpy = jest.spyOn(os, "uptime").mockReturnValue(UPTIME);
+
+      // when
+      const serverInfo = infoService.info(memory, name, uptime);
+
+      // then
+      expect(uptimeSpy).toHaveBeenCalled();
+      expect(serverInfo.hostName).toBe(HOST_NAME);
+      expect(serverInfo.uptime).toBe(UPTIME);
+      expect(serverInfo.freeMemory).toBeUndefined();
+      expect(serverInfo.totalMemory).toBeUndefined();
+    });
+
+    it(`should all info`, () => {
+      // given
+      memory = true;
+      name = true;
+      uptime = true;
+
+      const freememSpy = jest.spyOn(os, "freemem").mockReturnValue(FREE_MEMORY);
+      const totalmemSpy = jest.spyOn(os, "totalmem").mockReturnValue(TOTAL_MEMORY);
+      const uptimeSpy = jest.spyOn(os, "uptime").mockReturnValue(UPTIME);
+      mockedHostnameFn.mockReturnValue("TEST");
+
+      // when
+      const serverInfo = infoService.info(memory, name, uptime);
+
+      // then
+      expect(freememSpy).toHaveBeenCalled();
+      expect(totalmemSpy).toHaveBeenCalled();
+      expect(uptimeSpy).toHaveBeenCalled();
+      expect(serverInfo.freeMemory).toBe(FREE_MEMORY);
+      expect(serverInfo.totalMemory).toBe(TOTAL_MEMORY);
+      expect(serverInfo.hostName).toBe(HOST_NAME);
+      expect(serverInfo.uptime).toBe(UPTIME);
+    });
+  });
+});
