82eb20349f16b3e2c9b879e74cbab1a644924077
gingaminga <cjhwaan@gmail.com>
2024-01-15 15:07:36 +0900

    🚑 Fix: 경로 오타 수정

--- jest.config.js
+++ jest.config.js
@@ -96,7 +96,7 @@ const config = {
     "^@dto/(.*)$": "<rootDir>/src/dto/$1",
     "^@loaders/(.*)$": "<rootDir>/src/loaders/$1",
     "^@middlewares/(.*)$": "<rootDir>/src/middlewares/$1",
-    "^@my-types/(.*)$": "<rootDir>/src/src/types/$1",
+    "^@my-types/(.*)$": "<rootDir>/src/types/$1",
     "^@routes/(.*)$": "<rootDir>/src/routes/$1",
     "^@services/(.*)$": "<rootDir>/src/services/$1",
     "^@utils/(.*)$": "<rootDir>/src/utils/$1",
